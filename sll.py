1006b74ea3cccb9fdccf9fd2690da4ec3402098d
cahillbr <cahillbr@oregonstate.edu>
2023-02-14 08:31:36 -0800

    k

--- sll.py
+++ sll.py
@@ -16,13 +16,11 @@ class SLLException(Exception):
     """
     pass
 
-
 class SLNode:
     """
     Singly Linked List Node class
     DO NOT CHANGE THIS CLASS IN ANY WAY
     """
-
     def __init__(self, value: object) -> None:
         self.next = None
         self.value = value
